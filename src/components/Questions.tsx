7206c308ffef4e641625c49cd1421f10f8ef27bc
RonnieTz <ronis.tzol@gmail.com>
2023-07-03 09:55:29 +0100

    fixed correct answer

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -10,6 +10,9 @@ import {
   RadioGroup,
   Typography,
 } from "@mui/material";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
 import { decode } from "html-entities";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -21,6 +24,7 @@ import { store } from "../redux/store";
 import { changeGameStarted } from "../redux/options";
 
 const Questions = () => {
+  const [correctAnswers, setCorrectAnswers] = useState(0);
   const data = useSelector((state: RootState) => state.questions);
   const { answerChecked } = useSelector((state: RootState) => state.options);
   const theme = useTheme();
@@ -39,6 +43,7 @@ const Questions = () => {
   const onCheck = () => {
     if (data[activeStep].correct === checked) {
       setResult("Correct Answer");
+      setCorrectAnswers((prev) => prev + 1);
     }
     if (data[activeStep].correct !== checked && checked) {
       setResult("Wrong Answer");
@@ -48,8 +53,38 @@ const Questions = () => {
       dispatch(changeAnswerChecked(true));
     }
   };
+
+  const onNewGame = () => {
+    store.dispatch(changeGameStarted(false));
+    setChecked("");
+    dispatch(changeAnswerChecked(false));
+    setCorrectAnswers(0);
+  };
   return (
-    <Box sx={{ maxWidth: 600, flexGrow: 1 }}>
+    <Box position={"relative"} sx={{ maxWidth: 600, flexGrow: 1 }}>
+      {activeStep === maxSteps - 1 && answerChecked && (
+        <Box
+          zIndex={1}
+          position={"absolute"}
+          bgcolor={"greenyellow"}
+          width={"70%"}
+          height={"30%"}
+          top={"12%"}
+          left={"15%"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexDirection={"column"}
+        >
+          <Typography variant="h4">
+            Congratulations{" "}
+            <CelebrationIcon color="success" fontSize={"large"} />
+          </Typography>
+          <Typography marginRight={3} marginTop={2} variant="h6">
+            {`You got ${correctAnswers}/${data.length}`}
+          </Typography>
+        </Box>
+      )}
       <Paper
         square
         elevation={0}
@@ -83,6 +118,30 @@ const Questions = () => {
                   control={<Radio color="success" />}
                   label={decode(item)}
                 />
+                {data[activeStep].correct === checked &&
+                  checked === item &&
+                  answerChecked && (
+                    <DoneIcon
+                      color="success"
+                      sx={{ top: 5, position: "relative" }}
+                    />
+                  )}
+                {data[activeStep].correct !== checked &&
+                  checked === item &&
+                  answerChecked && (
+                    <CloseIcon
+                      color="error"
+                      sx={{ top: 8, position: "relative" }}
+                    />
+                  )}
+                {data[activeStep].correct === item &&
+                  checked !== item &&
+                  answerChecked && (
+                    <DoneIcon
+                      color="success"
+                      sx={{ top: 5, position: "relative" }}
+                    />
+                  )}
               </Box>
             ))}
           </RadioGroup>
@@ -119,7 +178,7 @@ const Questions = () => {
         backButton={null}
       />
       <Button
-        onClick={() => store.dispatch(changeGameStarted(false))}
+        onClick={onNewGame}
         variant="contained"
         sx={{ marginTop: 5 }}
         color="secondary"
